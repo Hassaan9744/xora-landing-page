14251cedaf69d251988643f4f2def218ebdcfacd
Hassaan9744 <mhahmad9744@gmail.com>
2024-10-30 15:34:40 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Download from "./sections/download";
+import Faq from "./sections/faq";
+import Feautres from "./sections/feautres";
+import Footer from "./sections/footer";
+import Header from "./sections/header";
+import Hero from "./sections/hero";
+import Pricing from "./sections/pricing";
+import Testimonials from "./sections/testimonials";
+
+export default function App() {
+  console.log("first");
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Feautres />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </main>
+  );
+}
